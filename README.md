f8b190cdb44cfc562b47ec033f555e4ffedb2617
eviau <37245459+eviau@users.noreply.github.com>
2018-08-28 10:30:23 -0400

    readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+I adapted OpenAI gym reinforcement learning framework into an algorithm that tries to learn what the user considers to be art, for a 100x100 pixels image.
+
+Repo can be found at [Github](https://github.com/eviau/IsItArtRL).
+
+You can read my thoughts on this project on my [blog](https://eviau.github.io/blog/IsItArtRL/isitartrl.html)
+
+I have made a smaller version where the algo only learns the colour red, as a sanity check that the implemntation is working. Code can be given on demand :)
+
+Next step: make it ~live~.
--- gym-isitartrl/gym_isitartrl/envs/isitartrl_env.py
+++ gym-isitartrl/gym_isitartrl/envs/isitartrl_env.py
@@ -7,6 +7,7 @@ from gym.utils import seeding
 import numpy as np
 
 from PIL import Image
+import random
 
 
 class isitartrlEnv(gym.Env):
@@ -18,7 +19,7 @@ class isitartrlEnv(gym.Env):
         self.TOTAL_TIME_STEPS = 1000000
         self.curr_step = -1
         self.action_space = spaces.MultiDiscrete(
-            [100,100,255,255,255])
+            [100, 100, 255, 255, 255])
         low = np.array([0.0])
         high = np.array([self.TOTAL_TIME_STEPS])
 
@@ -39,7 +40,7 @@ class isitartrlEnv(gym.Env):
         self._take_action(action)
         reward = self._get_reward()
         ob = self._get_state()
-        return ob, reward, self.is_picture_art,{}
+        return ob, reward, self.is_picture_art, {}
 
     def _take_action(self, action):
         self.action_episode_memory[self.curr_episode].append(action)
@@ -48,30 +49,30 @@ class isitartrlEnv(gym.Env):
         y = action[1]
         r = action[2] + 255
         b = action[3] + 255
-        g = action[4]  +255
+        g = action[4] + 255
 
-        self.current_painting[x,y] = [r,b,g]
+        self.current_painting[x, y] = [r, b, g]
         print(action)
 
         self.is_picture_art = self._eval_is_pict_art()
-
-
 
     def _eval_is_pict_art(self):
         print("is this picture art ?")
         image = Image.fromarray(self.current_painting)
         image.show()
-        temp = input('Enter 1 if it art, 0 if not:') 
-        if (temp==1):
+
+        temp = input('Enter 1 if it art, 0 if not:')
+        temp = random.randint(0, 1)
+        if (temp == 1):
             self.is_picture_art = True
-        elif (temp==1):
+        elif (temp == 1):
             self.is_picture_art = False
 
     def _get_reward(self):
         if self.is_picture_art:
-            return 10
+            return 10000
         else:
-            return 0
+            return -100
 
     def reset(self):
         """
